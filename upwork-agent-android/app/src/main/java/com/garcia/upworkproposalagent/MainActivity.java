34073cb3c69ec58c0041313b44a1fe673741da85
Garcia247 <studentreliefeduconsult@gmail.com>
2026-09-12 05:29:51 -0700

    Make Android Upwork agent fully offline

--- upwork-agent-android/app/src/main/java/com/garcia/upworkproposalagent/MainActivity.java
+++ upwork-agent-android/app/src/main/java/com/garcia/upworkproposalagent/MainActivity.java
@@ -1,24 +1,17 @@
 package com.garcia.upworkproposalagent;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    private static final int FILE_CHOOSER_REQUEST = 1001;
     private WebView webView;
-    private ValueCallback<Uri[]> fileCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         webView = new WebView(this);
         setContentView(webView);
 
@@ -29,53 +22,13 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
-        settings.setMediaPlaybackRequiresUserGesture(false);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-                if (fileCallback != null) {
-                    fileCallback.onReceiveValue(null);
-                }
-                fileCallback = filePathCallback;
-                Intent intent = fileChooserParams.createIntent();
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                try {
-                    startActivityForResult(intent, FILE_CHOOSER_REQUEST);
-                } catch (Exception e) {
-                    fileCallback = null;
-                    return false;
-                }
-                return true;
-            }
-        });
 
         if (savedInstanceState == null) {
-            webView.loadUrl("https://upwork-proposal-intelligence-agent.vercel.app/");
+            webView.loadUrl("file:///android_asset/index.html");
         } else {
             webView.restoreState(savedInstanceState);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FILE_CHOOSER_REQUEST && fileCallback != null) {
-            Uri[] results = null;
-            if (resultCode == RESULT_OK && data != null) {
-                if (data.getClipData() != null) {
-                    int count = data.getClipData().getItemCount();
-                    results = new Uri[count];
-                    for (int i = 0; i < count; i++) {
-                        results[i] = data.getClipData().getItemAt(i).getUri();
-                    }
-                } else if (data.getData() != null) {
-                    results = new Uri[]{data.getData()};
-                }
-            }
-            fileCallback.onReceiveValue(results);
-            fileCallback = null;
         }
     }
 
